f8bca23dae3a193ff0added1ee9ce692f4798517
Brandon Elam Barker <beb82@cornell.edu>
2018-10-02 21:13:16 +0000

    tentative fix for appropriate namespacing; 0.0.3

---
+++ api/js/src/edu/cornell/ncrn/ced2ar/service/api.scala
@@ -0,0 +1,6 @@
+package edu.cornell.ncrn.ced2ar.service
+
+import scala.scalajs.js.annotation._
+
+@JSExportTopLevel("CED2AR")
+object api extends Ced2arApi
--- api/js/src/package.scala
+++
@@ -1,6 +0,0 @@
-package edu.cornell.ncrn.service.api
-import scala.collection.immutable.ListMap
-import scala.scalajs.js.annotation._
-
-@JSExportTopLevel("CED2AR")
-object api extends Ced2arApi 
